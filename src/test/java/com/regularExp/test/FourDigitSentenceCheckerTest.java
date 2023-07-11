5c567fef774ec0a54fe1d74ce6959fc7ab546e74
HeinThantAung <heinthant098@gmail.com>
2023-07-11 16:24:02 +0630

    不必要なインポートの削除

--- src/test/java/com/regularExp/test/FourDigitSentenceCheckerTest.java
+++ src/test/java/com/regularExp/test/FourDigitSentenceCheckerTest.java
@@ -3,8 +3,6 @@ package com.regularExp.test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
-
 import com.regularExp.FourDigitSentenceChecker;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
